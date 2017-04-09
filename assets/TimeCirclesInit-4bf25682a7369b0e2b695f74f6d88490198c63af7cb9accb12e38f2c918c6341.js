9accd20428d4bc0daa2e4f5d918e57981d608189
fasol <remi.parrot@gmail.com>
2017-04-09 14:46:16 +0200

    maj date compte-à-rebourt ios

--- assets/TimeCirclesInit-4bf25682a7369b0e2b695f74f6d88490198c63af7cb9accb12e38f2c918c6341.js
+++ assets/TimeCirclesInit-4bf25682a7369b0e2b695f74f6d88490198c63af7cb9accb12e38f2c918c6341.js
@@ -31,7 +31,8 @@ $(".clock").TimeCircles({
 var date1 = new Date();
 var datadate = $("#clock_day").attr( 'data-date' );
 var t = datadate.split(/[- :]/);
-var date2 = new Date(t[0],t[1]-1,t[2],t[3],t[4],t[5]);
+var date2 = new Date(t[0],t[1],t[2],t[3],t[4],t[5]);
+window.alert(datadate);
 var diff = date2 - date1;
 var days = Math.floor(diff / 1000 / 60 / 60 / 24);
 
